75ad4772651e34f6ea17d82c019782b8aae3aad9
c-bre <fantome.bee@gmail.com>
2025-09-08 15:15:59 -0500

    Add files via upload

---
+++ data-types1.swift
@@ -0,0 +1,19 @@
+// Examples of constants
+let theAnnswerToTheUltimateQuestion = 42
+let pi = 3.14159
+let myName = "John Smith"
+
+// Examples of variables
+var currentTemperatureInCelcius = 27
+var myAge = 50
+var myLocation = "home"
+
+// Example of a Boolean constant
+let isRaining = true
+isRaining = false
+
+//Using type annotaiton to a specify a type
+var restaurantRating: Double = 3
+
+//Type Safety
+restaurantRating = "Good"
