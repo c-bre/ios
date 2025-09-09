420655df8134d4de3cbcb6a18be1c2e4f90996f5
c-bre <fantome.bee@gmail.com>
2025-09-08 19:43:58 -0500

    Add files via upload

---
+++ strings.swift
@@ -0,0 +1,15 @@
+// Performing string operations
+// You can join two strings together using the '+' operator
+
+// You can concantenate two strings using the '+' operator
+let greeting = "Good" + " Morning"
+print(greeting)
+
+// You can cast an interger or real as a string or concantenate it with another string
+let rating = 3.5
+var ratingResult = "The restaurant rating is " + String(rating)
+print(ratingResult)
+
+// You can also use string interpolation
+ratingResult = "The restaurant rating is \(rating)."
+print(ratingResult)
