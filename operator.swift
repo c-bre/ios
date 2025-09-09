f6e03d14cb67443c3c65497dfa62ca25faaa0e08
c-bre <fantome.bee@gmail.com>
2025-09-08 19:02:57 -0500

    Add files via upload

---
+++ operator.swift
@@ -0,0 +1,53 @@
+// + Addition
+// - Substraction
+// * Multiplication
+// / Division
+
+// Operations with Numbers
+// (+) operator for Addition
+let sum = 23 + 20
+
+//(-) operator for Subtraction
+let result = 32 - sum
+
+// (*) operator for Multiplication
+let total = result * 5
+
+// (/) operator for Division 
+let divide = total/10
+
+print(sum)
+print(result)
+print(total)
+print(divide)
+
+//Operators can only work with the operands of the same type
+let a = 12
+let b = 12.0
+//let c = a + b
+
+let c = Double(a) + b
+
+// Using compound assignment Operators
+
+// += Adds a value and assigns the result to the variable
+// -= Subtracts the value and assigns the result to the variable
+// *= Multiplies with the value and assigns the result to the variable
+// /= Divides with the value and assigns the result to the variable
+
+// compound assignment Operators
+
+var aa = 1
+
+print (aa)
+
+aa += 2
+//aa is now equal to 3
+
+print (aa)
+
+aa -= 1
+//aa is now equal to 2
+
+print (aa)
+
