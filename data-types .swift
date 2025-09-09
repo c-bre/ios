54e17da47fba0f08bf1391624ad939fafc1b580c
c-bre <fantome.bee@gmail.com>
2025-09-08 19:00:17 -0500

    Add files via upload

---
+++ data-types .swift
@@ -0,0 +1,27 @@
+// Examples of constants
+let theAnswerToTheUltimateQuestion = 42
+let pi = 3.14159
+let myName = "John Smith"
+
+print("The meaning of life" , theAnswerToTheUltimateQuestion)
+print("What is the value of pi?", pi)
+print("My name is: ", myName)
+ 
+// Examples of variables
+var currentTemperatureInCelcius = 27
+var myAge = 50
+var myLocation = "home"
+
+print("What is the Temperature" , currentTemperatureInCelcius)
+print("I am", myAge, "years old")
+print("I am currently at my", myLocation)
+
+// Example of a Boolean constant
+let isRaining = true
+isRaining = false
+
+//Eample of a Boolean constant- correct version
+var isRaining = true
+print(isRaining)
+isRaining = false
+print(isRaining)
