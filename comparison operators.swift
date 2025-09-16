6748d00e7fbbfedeb45afd8163d60747c67fd8e5
c-bre <fantome.bee@gmail.com>
2025-09-16 09:39:37 -0500

    Add files via upload

---
+++ comparison operators.swift
@@ -0,0 +1,25 @@
+// Using comparison Operators
+// Operators     Descrption
+// ==			 Equal to
+// !=			 Not Equal to
+// >			 Greater than
+// <			 Less than
+// >= 			 Greater than or Equal to
+// <= 			 Less than or Equal to
+
+
+// comparison Operators
+// 1 == 1  // Equal to, true because 1 is Equal to 1
+// 2 != 1  // not Equal to, true because 2 is not Equal to 1
+// 2 > 1  // Greater than, true because 2 is Greater than 1
+// 1 < 2  // less than, true because 1 is Less than 2
+// 1 >= 1 // Greater than or Equal to, true because 1 is Greater than or Equal to 1
+// 2 <= 1 // Less or Equal, false because 2 is Not Less than or Equal to 1
+
+print(1 == 1)  // Equal to, true because 1 is Equal to 1
+print(2 != 1)  // not Equal to, true because 2 is not Equal to 1
+print(2 > 1)  // Greater than, true because 2 is Greater than 1
+print(1 < 2)  // less than, true because 1 is Less than 2
+print(1 >= 1) // Greater than or Equal to, true because 1 is Greater than or Equal to 1
+print(2 <= 1) // Less or Equal, false because 2 is Not Less than or Equal to 1
+
